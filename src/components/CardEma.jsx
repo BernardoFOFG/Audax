2300087fe1a8bb00a2aecfa71c88e02b9251ad7b
BernardoFOFG <bernardofofg@gmail.com>
2022-10-28 00:14:53 -0300

    dia 4.1

--- src/components/CardEma.jsx
+++ src/components/CardEma.jsx
@@ -2,11 +2,11 @@ import "../styles/main.css";
 
 export function CardEma(props) {
   return (
-    <div className="flex flex-col xsm:pt-[35px] md:pt-[50px] xsm:w-[363px] md:h-[595px] xsm:h-[450px] bg-white text-primary rounded-lg px-8 items-center text-center">
+    <div className="flex flex-col xsm:pt-[35px] md:pt-[50px] xsm:w-[363px] md:w-[463px] md:h-[595px] xsm:h-[450px] bg-white text-primary rounded-lg px-8 items-center text-center">
       <h3 className="font-bold lg:text-md leading-7 mb-4">{props.title}</h3>
       <p className="font-normal lg:text-xsm leading-6">{props.description}</p>
       <img
-        className="h-[377px] lg:w-[277px] flex items-center"
+        className="h-[377px] xsm:w-[277px] md:w-[444px] flex items-center"
         src={props.src}
         alt={props.alt}
       />
